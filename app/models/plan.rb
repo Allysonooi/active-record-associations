6b9fd38b0cef9a3a9fb9c43b8a9d72c225128ae2
Allyson Ooi <allysonooi28@gmail.com>
2016-05-24 13:10:40 +0800

    Add has_many :plusrs to User and Plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,5 @@
 class Plan < ActiveRecord::Base
   belongs_to :admin
+  has_many :plusrs
+  has_many :users, through: :plusrs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   belongs_to :admin
+  has_many :plusrs
+  has_many :plans, through: :plusrs
 end
